e2304aa9118b4d8a4f85e0d92d184546834b2c6e
pss214 <bak10175@gmail.com>
2024-03-21 09:41:41 +0900

    0320 mentoring content Upload

---
+++ mentoring/src/java_0320/A.java
@@ -0,0 +1,15 @@
+package java_0320;
+
+public class A {
+    public static void main(String[] args) {
+        char c = 'a';
+        int i = 10;
+        double d = 3.14;
+        String s = "박성수";
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
---
+++ mentoring/src/java_0320/B.java
@@ -0,0 +1,15 @@
+package java_0320;
+
+public class B {
+    public static void main(String[] args) {
+        char h = 'J';
+        int i = 100;
+        double d = 37.26;
+        String m = "java멘토링";
+
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(m);
+    }
+}
